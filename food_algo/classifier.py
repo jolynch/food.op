99f8fd45d3f99340379f49b6fb9a7fd6fd1c98d1
Joseph Lynch <jolynch@mit.edu>
2013-07-25 17:01:49 -0700

    More work

---
+++ food_algo/classifier.py
@@ -0,0 +1,3 @@
+from sklearn.ensemble import GradientBoostingClassifier
+
+def 
--- web.py
+++ web.py
@@ -1,7 +1,34 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 import json
+import os
+
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vote = db.Column(db.Integer)
+    user_id = db.relationship('User',
+            backref=db.backref('votes', lazy='dynamic'))
+    recipe_id = db.relationship('Recipe',
+            backref=db.backref('votes', lazy='dynamic'))
+
+class Recipe(db.Mode):
+    id = db.Column(db.Integer, primary_key=True)
+    wiki_id = db.Column(db.Integer)
 
 @app.route('/')
 def index():
